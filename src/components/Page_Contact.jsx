388e9a9865c045163e7682e05efb31aba115293a
valuecom <devs@valuecom.gr>
2023-04-25 11:40:44 +0300

    changes

--- src/components/Page_Contact.jsx
+++ src/components/Page_Contact.jsx
@@ -36,9 +36,35 @@ const Page_Contact = (props) => {
         let doSubmit = true;
         if ( !validateName(form_name) ) doSubmit = false;
 
-        console.log(doSubmit);
-        console.log('handleContactSubmit');
-        console.log(form_name, form_email, form_message);
+        const data = { 'test':'test123123'} ;
+
+        const json_url = "http://localhost/githubVC/newsite2023.valuecom.gr/wp-json/contact/v1/send";
+        fetch(json_url,{
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+              // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: "follow", // manual, *follow, error
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
+        .then(response => {
+            return response.json();
+        }).then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+        // console.log(doSubmit);
+        // console.log('handleContactSubmit');
+        // console.log(form_name, form_email, form_message);
 
     }
 
