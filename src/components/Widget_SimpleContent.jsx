cf89ba794acf03458f7b8da94228e79cba93a9c2
valuecom <devs@valuecom.gr>
2023-05-10 12:04:04 +0300

    tests

--- src/components/Widget_SimpleContent.jsx
+++
@@ -1,9 +0,0 @@
-const Widget_SimpleContent = (props) => (  
-    <section>
-        <div className="container-xxl pb-5 px-5">
-            <div className="px-md-5 mx-lg-5" dangerouslySetInnerHTML = {{ __html: props.contentHTML }} ></div>
-        </div>
-    </section>
-);
-
-export default Widget_SimpleContent;
